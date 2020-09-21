d6961c2261d7c32cea00a33a1fcd599e311ab5a5
Sebastian Key <sebastianwkey@gmail.com>
2020-09-21 11:26:39 -0700

    cleanup

--- requirejs-config.js
+++ requirejs-config.js
@@ -2,14 +2,14 @@ requirejs.config({
     baseUrl: '',
 
     paths: {
-       "webrtc-sdk" : "./external/webrtc-sdk",
+       "webrtc-sdk" : "./external/webrtcsdk.min",
        jquery: './external/jquery',
        underscore: './external/lodash'
     }
 });
 
 require(["webrtc-sdk"], function () {
-    console.log("(require): webrtcsdk loaded");
+    console.log("(require): webrtcsdk.min loaded");
     require(['example'], function() {
         console.log("(require): example webrtc client app loaded");
     });
